494608fc5655b4436db7c34be1aebe1c92f5b33a
Joshua Wainaina Thuo <joshuawainaina31@gmail.com>
2025-09-25 09:25:36 +0300

    First Deploy to thebrand

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2485011266"
+    serviceWorkerVersion: "3549165954"
   }
 });
